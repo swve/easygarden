c29933584a5f6dfc9ae9d0f97c58d5930cc3e349
swve <bdswve@gmail.com>
2021-05-26 19:59:42 +0200

    inject task model

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Task, TaskDocument } from "./tasks/entities/task.entity";
 
 @Injectable()
 export class AppService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 }
