2858ee766f1699f9dfe848fffc7aa20a83e1ae8c
swve <bdswve@gmail.com>
2021-05-26 19:55:09 +0200

    init mongodb & task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CommunityModule } from './community/community.module';
-import { HealthModule } from './health/health.module';
-import { TasksModule } from './tasks/tasks.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CommunityModule } from "./community/community.module";
+import { HealthModule } from "./health/health.module";
+import { TasksModule } from "./tasks/tasks.module";
+import { MongooseModule } from "@nestjs/mongoose";
+
+
+// DB Crendentials
+const DB_CREDENTIALS = process.env.MONGO_CREDENTIALS || '';
+const DB_HOST = process.env.MONGO_HOST || 'localhost';
+const DB_PORT = process.env.MONGO_PORT || '27017';
+const DB_NAME = process.env.MONGO_DB_NAME || 'easygarden';
+const DB_CONNECTION_STRING = `mongodb://${DB_CREDENTIALS}${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 @Module({
-  imports: [CommunityModule, HealthModule, TasksModule],
+  imports: [
+    CommunityModule,
+    HealthModule,
+    TasksModule,
+    MongooseModule.forRoot(DB_CONNECTION_STRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,18 @@
-export class Task {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  breed: string;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
