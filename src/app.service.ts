5bb9462d9401b924caaa66d64d4d65464b7d99a8
swve <bdswve@gmail.com>
2021-06-01 15:17:40 +0200

    add trefle mock

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
 
 @Injectable()
 export class AppService {
-
   getHello(): string {
     return "Bonjour nous sommes le groupe EasyGarden 🏡";
+  }
+
+  /**
+   * A service that simulate what something
+   * like the Trefle API would return
+   * @returns Array of Plants
+   */
+  trefle() {
+    
+    return [
+      {
+        id: "tmt-2332098",
+        name: "Tomate",
+        family: "Solanacées",
+        best_temp: "20",
+        time_to_grow: "30",
+      },
+      {
+        id: "or-232098",
+        name: "Orange",
+        family: "Rutacées",
+        best_temp: "45",
+        time_to_grow: "20",
+      },
+    ];
   }
 }
